08cc67ca1f477714894ab50f9662f05ac44e7f61
Samuel Willian Alves Wu <samuelwuw@hotmail.com>
2020-09-04 18:59:07 -0300

    Crud para movies

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Cadastro.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int NumTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = NumTimes;
+
+            return View();
+        }
+    }
+}
