7d8255f3cb4f8f36fffdd7cf83345525b9f2feff
Samuel Willian Alves Wu <samuelwuw@hotmail.com>
2020-09-09 11:38:28 -0300

    mecanismo de pesquisa

---
+++ Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Cadastro.Data;
+using System;
+using System.Linq;
+
+namespace Cadastro.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Lord Of The Rings Part 1",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Fantasy",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Melhor programador de c# do mundo ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "De melhor a pior",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Filme 2",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
